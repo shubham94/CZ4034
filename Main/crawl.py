ec85d060c2be284dc76e754f9eca35e0cd65dc90
Shubham <shubham003@e.ntu.edu.sg>
2016-03-23 12:36:18 +0800

    Dynamic Crawling

--- Main/crawl.py
+++ Main/crawl.py
@@ -3,84 +3,104 @@ import json
 import requests
 import datetime
 
-STATS_FILE_NAME = "stats_health_insurance.csv"
-JSON_FILE_NAME = "search_health_insurance"
+from Main.Utility.MySQL import MySQL
 
-prefix = "http://api.nytimes.com/svc/search/v2/articlesearch.json"
-# fq = "fq=news_desk:(\"health\")"
-q = "q=health+insurance"
-fq = q
-sort = "sort=newest"
-page = "page="
-key1 = "api-key=bc6f4a013b593ac80ff7f31de9c52b80:11:74279314"
-key2 = "api-key=a52da62103b0deaf1a70d42c8ae09038:2:74279314"
-key3 = "api-key=04f0794217e078a662116b6a4486d18e:6:74279314"
-key = key1
+mysql_object = MySQL()
 
-count = 1
+"""
+Health - Health
+Health & Fitness - Health&Fitness
+Fitness & Nutrition - Health/Fitness Nutrition
+Men's & Health - Men & Health
+Women's Health - Women's Health
+"""
+# Replace this with function call or something.
+crawl_term = "Health"
+fq = ""
+query = ""
+if (crawl_term == "Health"):
+    query = "SELECT last_date,count from CRAWL_DATE WHERE news_desk='Health'"
+    news_desk = "Health"
+    fq = "fq=news_desk:(\"Health\")"
+    file_name = "health"
+elif (crawl_term == "Health & Fitness"):
+    query = "SELECT last_date,count from CRAWL_DATE WHERE news_desk='Health&Fitness'"
+    news_desk = "Health&Fitness"
+    fq = "fq=news_desk:(\"Health&Fitness\")"
+    file_name = "health_fitness"
+elif (crawl_term == "Fitness & Nutrition"):
+    query = "SELECT last_date,count from CRAWL_DATE WHERE news_desk='Health/Fitness Nutrition'"
+    news_desk = "Health/Fitness Nutrition"
+    fq = "fq=news_desk:(\"Health/Fitness Nutrition\")"
+    file_name = "health_nutrition"
+elif (crawl_term == "Men's & Health"):
+    query = "SELECT last_date,count from CRAWL_DATE WHERE news_desk='Men & Health'"
+    news_desk = "Men & Health"
+    fq = "fq=news_desk:(\"Men & Health\")"
+    file_name = "health_men_health"
+else:
+    query = "SELECT last_date,count from CRAWL_DATE WHERE news_desk='Women & Health'"
+    news_desk = "Women's Health"
+    fq = "fq=news_desk:(\"Women's Health\")"
+    file_name = "health_women_health"
 
-days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+query = query.encode('utf-8')
+data = mysql_object.execute_query(query)
 
-url = prefix + "?&" + fq + "&" + "sort=newest" + "&" + page + str(0) + "&" + key
-resp = requests.get(url)
-newest_year = (resp.json()["response"]["docs"][0]["pub_date"])[0:4]
-url = prefix + "?&" + fq + "&" + "sort=oldest" + "&" + page + str(0) + "&" + key
-resp = requests.get(url)
-oldest_year = (resp.json()["response"]["docs"][0]["pub_date"])[0:4]
+last_date = data[0][0]
+count_num = data[0][1]
+JSON_FILE_NAME = "fq_" + file_name
+
 current_year = datetime.date.today().strftime('%Y')
-current_date = ((datetime.date.today() + datetime.timedelta(days=0)).strftime('%Y%m%d'))
+current_date_format = ((datetime.date.today() + datetime.timedelta(days=0)).strftime('%Y-%m-%d'))
+if ((str(last_date) == current_date_format)):
+    print("No new articles found")
+else:
+    current_date = ((datetime.date.today() + datetime.timedelta(days=0)).strftime('%Y%m%d'))
+    prefix = "http://api.nytimes.com/svc/search/v2/articlesearch.json"
+    # fq = "fq=news_desk:(\"health\")"
+    # q = "q=health+insurance"
+    # fq = "fq=news_desk:(\"" + crawl_term + "\")"
+    sort = "sort=newest"
+    page = "page="
+    key1 = "api-key=bc6f4a013b593ac80ff7f31de9c52b80:11:74279314"
+    key2 = "api-key=a52da62103b0deaf1a70d42c8ae09038:2:74279314"
+    key3 = "api-key=04f0794217e078a662116b6a4486d18e:6:74279314"
+    key = key1
 
-with open(STATS_FILE_NAME, "w") as stats:
-    stats.write("Begin Date, End Date, Hits, Pages\n")
-for year in range(int(newest_year), int(oldest_year) - 1, -1):
-    for month in range(1, 13):
-        for date in range(1, days[month - 1] + 1, 7):
-            mon = month
-            bd = date
-            ed = date
-            if (month < 10):
-                mon = "0" + str(month)
-            if (bd < 10):
-                bd = "0" + str(bd)
-            if (ed + 6 < 10):
-                ed = "0" + str(ed + 6)
-            elif (ed + 6 > days[month - 1]):
-                ed = days[month - 1]
-            else:
-                ed += 6
-            begin_date = "begin_date=" + str(year) + str(mon) + str(bd)
-            end_date = "end_date=" + str(year) + str(mon) + str(ed)
-            if (current_date < begin_date[11:]):
-                continue
-            if (end_date[9:] > current_date):
-                end_date = "end_date=" + current_date
-            url = prefix + "?&" + fq + "&" + sort + "&" + begin_date + "&" + end_date + "&" + page + "0" + "&" + key
-            print(url)
-            time.sleep(5)
+    url = prefix + "?&" + fq + "&" + "begin_date=" + str(
+        last_date.strftime('%Y%m%d')) + "&" "end_date=" + current_date + "&" + "sort=newest" + "&" + page + str(
+        0) + "&" + key
+    print (url)
+    resp = requests.get(url)
+    hits = resp.json()["response"]["meta"]["hits"]
+
+    pages = int(hits / 10)
+    if (pages * 10 < hits):
+        pages += 1
+    if (pages > 0):
+        print("Number of pages = " + str(pages))
+        for i in range(0, pages):
+            url = prefix + "?&" + fq + "&" + sort + "&" + last_date.strftime(
+                '%Y%m%d') + "&" + current_date + "&" + page + str(
+                i) + "&" + key
             resp = requests.get(url)
-            hits = resp.json()["response"]["meta"]["hits"]
-            pages = int(hits / 10)
-            if (pages * 10 < hits):
-                pages += 1
-            if (pages > 0):
-                print("Number of pages = " + str(pages))
-            with open(STATS_FILE_NAME, "a") as stats:
-                stats.write(
-                    str(year) + str(mon) + str(bd) + "," + str(year) + str(mon) + str(ed) + "," + str(hits) + "," + str(
-                        pages) + "\n")
-            for i in range(0, pages):
-                url = prefix + "?&" + fq + "&" + sort + "&" + begin_date + "&" + end_date + "&" + page + str(
-                    i) + "&" + key
-                resp = requests.get(url)
-                with open("../jsonFiles/" + JSON_FILE_NAME + str(count) + ".json", 'w') as jsonFile:
-                    json.dump(resp.json(), jsonFile)
-                print("Writing to file: " + JSON_FILE_NAME + str(count) + ".json")
-                print("Page = " + str(i) + " done")
-                if (count % 150 == 1):
-                    key = key1
-                elif (count % 150 == 51):
-                    key = key2
-                if (count % 150 == 101):
-                    key = key3
-                count += 1
-                time.sleep(5)
+            with open("../jsonFiles/" + JSON_FILE_NAME + str(count_num) + ".json", 'w') as jsonFile:
+                json.dump(resp.json(), jsonFile)
+            print("Writing to file: " + JSON_FILE_NAME + str(count_num) + ".json")
+            print("Page = " + str(i) + " done")
+            if (count_num % 150 == 1):
+                key = key1
+            elif (count_num % 150 == 51):
+                key = key2
+            if (count_num % 150 == 101):
+                key = key3
+            count_num += 1
+            time.sleep(5)
+        mysql_object.execute_query(
+            "UPDATE crawl_date set count = " + str(count_num) + " where news_desk = \"" + str(news_desk) + "\"")
+        mysql_object.execute_query(
+            "UPDATE crawl_date set last_date = \"" + str(current_date_format) + "\" where news_desk = \"" + str(
+                news_desk) + "\"")
+    else:
+        print("Corpus already up to date")
