e75f46b7a2eb60d3f1febbd67c839adac7089ef8
Shubham <shubham003@e.ntu.edu.sg>
2016-04-03 02:26:33 +0800

    Different Classfication Models

--- Main/Classification/classificationSvm.py
+++ Main/Classification/classificationSvm.py
@@ -1,4 +1,8 @@
 from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import BernoulliNB
 from sklearn import feature_extraction
 import string
 from Main.Utility.MySQL import MySQL
@@ -11,10 +15,10 @@ mysql_object = MySQL()
 mysql_object.use_database(database_name)
 
 sql = "(SELECT lead_paragraph, news_desk FROM " + table_name + " WHERE news_desk LIKE \"travel\" order by DocID asc  LIMIT 50) " \
-      "UNION " \
-      "(SELECT lead_paragraph, news_desk FROM " + table_name + " WHERE news_desk LIKE \"dining\" order by DocID asc LIMIT 50) " \
-      "UNION " \
-      "(SELECT lead_paragraph, news_desk FROM " + table_name + " WHERE news_desk LIKE \"Politics\" order by DocID asc LIMIT 50);"
+                                                               "UNION " \
+                                                               "(SELECT lead_paragraph, news_desk FROM " + table_name + " WHERE news_desk LIKE \"dining\" order by DocID asc LIMIT 50) " \
+                                                                                                                        "UNION " \
+                                                                                                                        "(SELECT lead_paragraph, news_desk FROM " + table_name + " WHERE news_desk LIKE \"Politics\" order by DocID asc LIMIT 50);"
 
 sql = sql.encode('utf-8')
 data = mysql_object.execute_query(sql)
@@ -40,22 +44,13 @@ for record in data:
         dict.extend(d)
 
 dict = filter(None, list(set(dict)))
-print dict
 
-print train_data
-model = SVC(kernel='linear', C=1, gamma=1)
-
-cv = feature_extraction.text.CountVectorizer(vocabulary=dict)
-X = cv.fit_transform(train_data).toarray()
-print categ
-model.fit(X, categ)
-model.score(X, categ)
-
+# TEST DATA
 sql = "(SELECT lead_paragraph, news_desk FROM " + table_name + " WHERE news_desk LIKE \"Travel\" order by DocID desc  LIMIT 25) " \
-      "UNION " \
-      "(SELECT lead_paragraph, news_desk FROM " + table_name + " WHERE news_desk LIKE \"dining\" order by DocID desc LIMIT 25) " \
-      "UNION " \
-      "(SELECT lead_paragraph, news_desk FROM " + table_name + " WHERE news_desk LIKE \"Politics\" order by DocID desc LIMIT 25);"
+                                                               "UNION " \
+                                                               "(SELECT lead_paragraph, news_desk FROM " + table_name + " WHERE news_desk LIKE \"dining\" order by DocID desc LIMIT 25) " \
+                                                                                                                        "UNION " \
+                                                                                                                        "(SELECT lead_paragraph, news_desk FROM " + table_name + " WHERE news_desk LIKE \"Politics\" order by DocID desc LIMIT 25);"
 
 sql = sql.encode('utf-8')
 data = mysql_object.execute_query(sql)
@@ -63,7 +58,44 @@ test_data = []
 for record in data:
     test_data.append(record[0])
 
-Y = cv.fit_transform(test_data).toarray()
-predicted = model.predict(Y)
+model1 = SVC(kernel='linear', C=1, gamma=1)
+model2 = LogisticRegression()
+model3 = GaussianNB()
+model4 = MultinomialNB()
+model5 = BernoulliNB()
 
-print(predicted)
+cv1 = feature_extraction.text.CountVectorizer(vocabulary=dict)
+cv2 = feature_extraction.text.TfidfVectorizer(vocabulary=dict)
+
+model_used = ["SVM", "LOGISTIC REGRESSION", "GAUSSIAN NB", "MULTINOMIAL NB", "BERNOULLI NB"]
+cv_used = ["COUNT VECTORIZER", "TFIDF VECTORIZER"]
+model_list = [model1, model2, model3, model4, model5]
+cv_list = [cv1, cv2]
+for counter_model in range(0, 5):
+    for counter_cv in range(0, 2):
+        model = model_list[counter_model]
+        cv = cv_list[counter_cv]
+        X = cv.fit_transform(train_data).toarray()
+        model.fit(X, categ)
+        model.score(X, categ)
+        Y = cv.fit_transform(test_data).toarray()
+        predicted = model.predict(Y)
+        j = 1
+        travel = 0
+        dining = 0
+        politics = 0
+        for i in predicted:
+            if (j > 0 and j < 26 and i == "Travel"):
+                travel += 1
+            elif (j > 25 and j < 51 and i == "Dining"):
+                dining += 1
+            elif (j > 50 and j < 73 and i == "Politics"):
+                politics += 1
+            j += 1
+        print("_______________________")
+        print("MODEL      : " + model_used[counter_model])
+        print("VECTORIZER : " + cv_used[counter_cv])
+        print("Travel     : " + str(travel) + "/25")
+        print("Dining     : " + str(dining) + "/25")
+        print("Politics   : " + str(politics) + "/23")
+        print
