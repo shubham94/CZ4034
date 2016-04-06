d3d0ec694ae2dc8b51382f7541263205a7d76fce
sanjanajayakumar <sanjana2902.sj@gmail.com>
2016-04-06 16:02:25 +0800

    "Sanjana indexing"

--- Main/indexTesting.py
+++ Main/indexTesting.py
@@ -1,9 +1,132 @@
 import pysolr, os, json
+from nltk.corpus import wordnet as wn
+from Main.lemmatization import lemmatization
+from itertools import chain
+import simplejson
+from urllib2 import urlopen
 
 solr = pysolr.Solr('http://localhost:8983/solr/test', timeout=10)
 
-# If sending JSON files directly into solr
+lem = lemmatization()
 
+query="obesity and heart"
+queryLst = lem.lemmatizeWord(lem.removeStopWords(query.lower().split(" ")))
+print "query list: \n"
+print queryLst
+resplst=set()
+url="http://localhost:8983/solr/test/select?q="
+
+#keyword AND search#############################
+for word in queryLst:
+    #url+="%2Bkeywords:*\:*" + word + "*+"
+    url+="%2Bkeywords:*\:*" + word[0].upper() + word[1:] + "*+"
+url = url[:-1]
+url+="&wt=json&rows=15"
+print "url: " + url
+
+conn = urlopen(url)
+rsp = simplejson.load(conn)
+print rsp
+for result in rsp["response"]["docs"]:
+     print(result)
+     resplst.add(result["docID"][0])
+
+##headline AND search #############################
+
+url="http://localhost:8983/solr/test/select?q="
+
+for word in queryLst:
+    #url+="%2Bheadline:*\:*" + word + "*+"
+    url+="%2Bheadline:*\:*" + word[0].upper() + word[1:] + "*+"
+
+url = url[:-1]
+url+="&wt=json&rows=15"
+print "url: " + url
+
+
+conn = urlopen(url)
+rsp = simplejson.load(conn)
+print rsp
+for result in rsp["response"]["docs"]:
+     print(result)
+     resplst.add(result["docID"][0])
+
+print resplst
+
+##keyword + headline OR #############################
+
+url="http://localhost:8983/solr/test/select?q="
+
+for word in queryLst:
+    #url+="headline:*\:*" + word + "*+" + "keywords:*\:*" + word + "*+"
+    url+="headline:*\:*" + word[0].upper() + word[1:] + "*+" + "keywords:*\:*" + word[0].upper() + word[1:] + "*+"
+
+url = url[:-1]
+url+="&wt=json&rows=15"
+print "url: " + url
+
+
+conn = urlopen(url)
+rsp = simplejson.load(conn)
+print rsp
+for result in rsp["response"]["docs"]:
+     print(result)
+     resplst.add(result["docID"][0])
+
+print resplst
+
+##phrase search #############################
+
+
+url="http://localhost:8983/solr/test/select?q=headline:*\:\""
+
+for word in queryLst:
+    #url+= word + "+"
+    url+=word[0].upper() + word[1:] + "+"
+
+url = url[:-1]
+url+="\"&wt=json&rows=15"
+print "url: " + url
+
+conn = urlopen(url)
+rsp = simplejson.load(conn)
+print rsp
+for result in rsp["response"]["docs"]:
+     print(result)
+     resplst.add(result["docID"][0])
+
+print resplst
+
+## SYNONYM headline OR search
+
+
+print "New list"
+for item in queryLst:
+    synonyms = wn.synsets(item)
+    lst = set(chain.from_iterable([word.lemma_names() for word in synonyms]))
+
+    url="http://localhost:8983/solr/test/select?q="
+    for word in lst:
+        #url+="%2Bheadline:*\:*" + word + "*+"
+        url+="headline:*\:*" + word[0].upper() + word[1:] + "*+"
+
+    url = url[:-1]
+    url+="&wt=json&rows=15"
+    print "url: " + url
+
+    conn = urlopen(url)
+    rsp = simplejson.load(conn)
+    print rsp
+    for result in rsp["response"]["docs"]:
+         print(result)
+         resplst.add(result["docID"][0])
+
+    print resplst
+
+
+print resplst.__len__()
+# If sending JSON files directly into solr
+"""
 
 # How you'd index data.
 solr.delete(q='*:*')
@@ -242,11 +365,14 @@ solr.commit()
 
 # Later, searching is easy. In the simple case, just a plain Lucene-style
 # query is fine.
-results = solr.search('*')
+
+results = solr.search('Obesity')
 for result in results:
     print(result)
 
+"""
 
+#(q='headline:*Obesity* AND keywords:*Obesity* AND wt=json AND rows=15')
 
 """
 solr.delete(q='*:*')
